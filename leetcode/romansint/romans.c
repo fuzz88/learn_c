5c804e833600ad3ae7c423441a6c99c4d2a6ae86
fuzz88 <ivan@oschepkov.ru>
2022-04-06 14:26:21 +0800

    work on romans

--- leetcode/romansint/romans.c
+++ leetcode/romansint/romans.c
@@ -1,13 +1,43 @@
 #include <stdio.h>
 #include <string.h>
 
+
+/* romanDigit converts single roman digit to decimal numeral */
+int romanDigit(char digit)
+{
+	switch (digit)
+	{
+		case 'M':
+			return 1000;
+		case 'D':
+			return 500;
+		case 'C':
+			return 100;
+		case 'L':
+			return 50;
+		case 'X':
+			return 10;
+		case 'V':
+			return 5;
+		case 'I':
+			return 1;
+	}
+}
+
+
 /* romanToInt converts a roman numeral repr. as string to integer. */
 int romanToInt(char * s)
 {	
 	int length;
+	int sum = 0;
+
 	printf("%s\n", s);
 	length = strlen(s);
-	return length;
+	for (int i = length - 1; i != -1; i--)
+	{
+		sum += romanDigit(s[i]);
+	}
+	return sum;
 }
 
 int main(void)
